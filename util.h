53a3299cbf11d903d062a10d9e90570e9204482c
H. Peter Anvin <hpa@zytor.com>
2019-04-02 21:23:34 -0700

    Add util.h from nightlight project; probably useful in general

---
+++ util.h
@@ -0,0 +1,18 @@
+#ifndef UTIL_H
+#define UTIL_H
+
+#include <stddef.h>
+#include <stdbool.h>
+#include <inttypes.h>
+#include <avr/eeprom.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <avr/sleep.h>
+#include <util/delay.h>
+
+#define likely(x)   __builtin_expect(!!(x), true)
+#define unlikely(x) __builtin_expect(!!(x), false)
+
+#endif /* UTIL_H */
+
